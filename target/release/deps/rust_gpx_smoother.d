facb5911a7c80a6725dbb0c62b0fd93dc30a00d6
AndrewJulioDEV <andrew.julio.tompsett@gmail.com>
2025-05-28 07:56:32 +0200

    Update - Almost there

--- target/release/deps/rust_gpx_smoother.d
+++ target/release/deps/rust_gpx_smoother.d
@@ -1,6 +1,7 @@
-C:\Users\Dzhu\Documents\GitHub\rust-gpx-smoother\target\release\deps\rust_gpx_smoother.exe: src\main.rs src\custom_smoother.rs
+C:\Users\Dzhu\Documents\GitHub\rust-gpx-smoother\target\release\deps\rust_gpx_smoother.exe: src\main.rs src\custom_smoother.rs src\universal_smoother.rs
 
-C:\Users\Dzhu\Documents\GitHub\rust-gpx-smoother\target\release\deps\rust_gpx_smoother.d: src\main.rs src\custom_smoother.rs
+C:\Users\Dzhu\Documents\GitHub\rust-gpx-smoother\target\release\deps\rust_gpx_smoother.d: src\main.rs src\custom_smoother.rs src\universal_smoother.rs
 
 src\main.rs:
 src\custom_smoother.rs:
+src\universal_smoother.rs:
